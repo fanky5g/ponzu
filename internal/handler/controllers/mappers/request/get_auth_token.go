563df4a044fbfa8444f28055900cac0eae733e90
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-02-09 07:02:42 +0100

    support auth token from Authorization header

--- internal/handler/controllers/mappers/request/get_auth_token.go
+++ internal/handler/controllers/mappers/request/get_auth_token.go
@@ -1,8 +1,29 @@
 package request
 
-import "net/http"
+import (
+	"net/http"
+	"strings"
+)
 
 func GetAuthToken(req *http.Request) string {
+	if cookieToken := getAuthTokenFromCookie(req); cookieToken != "" {
+		return cookieToken
+	}
+
+	authHeader := req.Header.Get("Authorization")
+	if authHeader == "" {
+		return ""
+	}
+
+	parts := strings.Split(authHeader, "Bearer ")
+	if len(parts) < 2 {
+		return ""
+	}
+
+	return parts[1]
+}
+
+func getAuthTokenFromCookie(req *http.Request) string {
 	// check if token exists in cookie
 	cookie, err := req.Cookie("_token")
 	if err != nil {
