edf6e41dec9cc7475bfbd70dc45d8509ee4c6074
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-03-08 07:46:34 +0100

    use name as label

--- internal/domain/services/management/editor/nested.go
+++ internal/domain/services/management/editor/nested.go
@@ -2,13 +2,14 @@ package editor
 
 func Nested(fieldName string, p interface{}, args *FieldArgs, fields ...Field) []byte {
 	name := fieldName
+
 	if name == "" && args != nil {
 		name = args.TypeName
 	}
 
 	tmpl := `
 		<fieldset class="col s12 ` + name + `" name="` + name + `">
-			<label class="active">` + fieldName + `</label>
+			<h6>` + name + `</h6>
 	`
 
 	for _, field := range fields {
