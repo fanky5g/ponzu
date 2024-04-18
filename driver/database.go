ad327a42edaddccf48ca75c3153bb88a1c0edf1c
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-04-18 07:48:26 +0200

    remove GetRepository Database method

--- driver/database.go
+++ driver/database.go
@@ -1,12 +1,10 @@
 package driver
 
 import (
-	"github.com/fanky5g/ponzu/entities"
 	"github.com/fanky5g/ponzu/tokens"
 )
 
 type Database interface {
 	GetRepositoryByToken(token tokens.RepositoryToken) Repository
-	GetRepository(entity entities.EntityStoreInterface) Repository
 	Close() error
 }
