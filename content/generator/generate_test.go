054548924ac25e72d1bf6a2aa9baeff27c5e090c
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-05-04 09:59:50 +0200

    refactor content generator to use generator package

--- content/generator/generate_test.go
+++ content/generator/generate_test.go
@@ -1,51 +1,71 @@
 package generator
 
 import (
+	"fmt"
 	"github.com/fanky5g/ponzu/content"
-	"github.com/fanky5g/ponzu/content/generator/types"
+	"github.com/fanky5g/ponzu/generator"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
 	"go/format"
+	"strings"
 	"testing"
 )
 
 type GenerateTestSuite struct {
 	suite.Suite
-	gt *generator
+	gt *contentGenerator
+}
+
+type testWriter struct{ buf []byte }
+
+func (writer *testWriter) Write(filePath string, buf []byte) error {
+	writer.buf = buf
+	var err error
+	if strings.HasSuffix(strings.TrimSuffix(filePath, ".tmpl"), ".go") {
+		writer.buf, err = format.Source(buf)
+		if err != nil {
+			return fmt.Errorf("failed to format template: %s", err.Error())
+		}
+	}
+
+	return nil
 }
 
 func (s *GenerateTestSuite) SetupSuite() {
 	var err error
-	s.gt, err = setupGenerator(Config{
-		Types: content.Types{},
-		Target: Target{
-			Path: Path{
+	s.gt, err = setupGenerator(generator.Config{
+		Target: generator.Target{
+			Path: generator.Path{
 				Root: "",
 				Base: "",
 			},
 			Package: "entities",
 		},
-	})
+	}, content.Types{})
 	if err != nil {
 		s.T().Fatal(err)
 	}
 }
 
 func (s *GenerateTestSuite) TestGenerateFieldCollection() {
-	t := &types.TypeDefinition{
-		Name:    "PageContentBlocks",
-		Label:   "Page Content Blocks",
-		Initial: "p",
-		ContentBlocks: []types.ContentBlock{
+	t := &generator.TypeDefinition{
+		Name:     "PageContentBlocks",
+		Label:    "Page Content Blocks",
+		Type:     generator.FieldCollection,
+		Metadata: generator.Metadata{MethodReceiverName: "p"},
+		Blocks: []generator.Block{
 			{
+				Type:     generator.ContentBlock,
 				TypeName: "ImageGallery",
 				Label:    "Image Gallery",
 			},
 			{
+				Type:     generator.ContentBlock,
 				TypeName: "ImageAndTextBlock",
 				Label:    "Image And Text Block",
 			},
 			{
+				Type:     generator.ContentBlock,
 				TypeName: "TextBlock",
 				Label:    "Text Block",
 			},
@@ -71,7 +91,7 @@ func (s *GenerateTestSuite) TestGenerateFieldCollection() {
 		if p == nil {
 			return nil
 		}
-	
+
 		return *p
 	}
 
@@ -149,51 +169,174 @@ func (s *GenerateTestSuite) TestGenerateFieldCollection() {
 		s.T().Fatal(err)
 	}
 
-	buf, err := s.gt.generate(content.TypeFieldCollection, t)
+	w := new(testWriter)
+
+	err = s.gt.Generate(t, w)
 	if assert.NoError(s.T(), err) {
-		assert.Equal(s.T(), expectedBuffer, buf)
+		assert.Equal(s.T(), expectedBuffer, w.buf)
 	}
 }
 
-func (s *GenerateTestSuite) TestGenerateType() {
-	t := &types.TypeDefinition{
-		Name:    "Author",
-		Label:   "Author",
-		Initial: "a",
-		Fields: []types.Field{
+func (s *GenerateTestSuite) TestGenerateContentType() {
+	t := &generator.TypeDefinition{
+		Type:  generator.Content,
+		Name:  "Author",
+		Label: "Author",
+		Metadata: generator.Metadata{
+			MethodReceiverName: "a",
+		},
+		Blocks: []generator.Block{
 			{
-				Name:              "Name",
-				Label:             "Name",
-				Initial:           "a",
-				TypeName:          "string",
-				JSONName:          "name",
-				ViewType:          "input",
-				IsReference:       false,
-				IsNested:          false,
-				ReferenceName:     "",
-				ReferenceJSONTags: []string{},
+				Type:     generator.Field,
+				Name:     "Name",
+				Label:    "Name",
+				TypeName: "string",
+				JSONName: "name",
 			},
 			{
-				Name:              "Age",
-				Label:             "Age",
-				Initial:           "a",
-				TypeName:          "int",
-				JSONName:          "age",
-				ViewType:          "input",
-				IsReference:       false,
-				IsNested:          false,
-				ReferenceName:     "",
-				ReferenceJSONTags: []string{},
+				Type:     generator.Field,
+				Name:     "Age",
+				Label:    "Age",
+				TypeName: "int",
+				JSONName: "age",
 			},
 		},
-		HasReferences: false,
 	}
 
 	expectedBuffer, err := format.Source([]byte(`
-	package entities
+package entities
 
 import (
-	"github.com/fanky5g/ponzu/content"
+	"fmt"
+	"github.com/fanky5g/ponzu/config"
+	"github.com/fanky5g/ponzu/content/editor"
+	"github.com/fanky5g/ponzu/content/item"
+	"reflect"
+)
+
+type Author struct {
+	item.Item
+
+	Name string ` + "`json:\"name\"`" + `
+	Age  int    ` + "`json:\"age\"`" + `
+}
+
+// MarshalEditor writes a buffer of views to edit a Author within the CMS
+// and implements editor.Editable
+func (a *Author) MarshalEditor(paths config.Paths) ([]byte, error) {
+	view, err := editor.Form(a,
+		paths,
+		// Take note that the first argument to these Input-like functions
+		// is the string version of each Author field, and must follow
+		// this pattern for auto-decoding and auto-encoding reasons:
+		editor.Field{
+			View: editor.Input("Name", a, map[string]string{
+				"label":       "Name",
+				"type":        "text",
+				"placeholder": "Enter the Name here",
+			}, nil),
+		},
+		editor.Field{
+			View: editor.Input("Age", a, map[string]string{
+				"label":       "Age",
+				"type":        "text",
+				"placeholder": "Enter the Age here",
+			}, nil),
+		},
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to render Author editor view: %s", err.Error())
+	}
+
+	return view, nil
+}
+
+func init() {
+	Content["Author"] = func() interface{} { return new(Author) }
+}
+
+// IndexContent determines if Author should be indexed for searching
+func (a *Author) IndexContent() bool {
+	return false
+}
+
+// GetSearchableAttributes defines fields of Author that should be indexed
+func (a *Author) GetSearchableAttributes() map[string]reflect.Type {
+	searchableAttributes := make(map[string]reflect.Type)
+	idField := "ID"
+	v := reflect.Indirect(reflect.ValueOf(a))
+	searchableAttributes[idField] = v.FieldByName(idField).Type()
+
+	t := v.Type()
+	for i := 0; i < v.NumField(); i++ {
+		field := v.Field(i)
+		fieldName := t.Field(i).Name
+
+		if fieldName != idField && field.Kind() == reflect.String {
+			searchableAttributes[fieldName] = field.Type()
+		}
+	}
+
+	return searchableAttributes
+}
+	`))
+
+	if err != nil {
+		s.T().Fatal(err)
+	}
+
+	w := new(testWriter)
+
+	err = s.gt.Generate(t, w)
+	if assert.NoError(s.T(), err) {
+		assert.Equal(s.T(), expectedBuffer, w.buf)
+	}
+}
+
+func (s *GenerateTestSuite) TestGeneratePlainType() {
+	t := &generator.TypeDefinition{
+		Type:  generator.Plain,
+		Name:  "Author",
+		Label: "Author",
+		Metadata: generator.Metadata{
+			MethodReceiverName: "a",
+		},
+		Blocks: []generator.Block{
+			{
+				Type:     generator.Field,
+				Name:     "Name",
+				Label:    "Name",
+				TypeName: "string",
+				JSONName: "name",
+				Definition: generator.BlockDefinition{
+					Title:       "Name",
+					Type:        "string",
+					IsArray:     false,
+					IsReference: false,
+				},
+			},
+			{
+				Type:     generator.Field,
+				Name:     "Age",
+				Label:    "Age",
+				TypeName: "int",
+				JSONName: "age",
+				Definition: generator.BlockDefinition{
+					Title:       "Age",
+					Type:        "int",
+					IsArray:     false,
+					IsReference: false,
+				},
+			},
+		},
+	}
+
+	expectedBuffer, err := format.Source([]byte(`
+package entities
+
+import (
+	"github.com/fanky5g/ponzu/generator"
 )
 
 type Author struct {
@@ -202,37 +345,45 @@ type Author struct {
 }
 
 func init() {
-	Definitions["Author"] = content.TypeDefinition{
-		Name:    "Author",
-		Label:   "Author",
-		Initial: "a",
-		Fields: []content.Field{
+	Definitions["Author"] = generator.TypeDefinition{
+		Type:  generator.Plain,
+		Name:  "Author",
+		Label: "Author",
+		Metadata: generator.Metadata{
+			MethodReceiverName: "a",
+		},
+		Blocks: []generator.Block{
 			{
+				Type:              generator.Field,
 				Name:              "Name",
 				Label:             "Name",
-				Initial:           "a",
 				TypeName:          "string",
 				JSONName:          "name",
-				ViewType:          "input",
-				IsReference:       false,
-				IsNested:          false,
 				ReferenceName:     "",
 				ReferenceJSONTags: []string{},
+				Definition: generator.BlockDefinition{
+					Title:       "Name",
+					Type:        "string",
+					IsArray:     false,
+					IsReference: false,
+				},
 			},
 			{
+				Type:              generator.Field,
 				Name:              "Age",
 				Label:             "Age",
-				Initial:           "a",
 				TypeName:          "int",
 				JSONName:          "age",
-				ViewType:          "input",
-				IsReference:       false,
-				IsNested:          false,
 				ReferenceName:     "",
 				ReferenceJSONTags: []string{},
+				Definition: generator.BlockDefinition{
+					Title:       "Age",
+					Type:        "int",
+					IsArray:     false,
+					IsReference: false,
+				},
 			},
 		},
-		HasReferences: false,
 	}
 }
 	`))
@@ -241,9 +392,11 @@ func init() {
 		s.T().Fatal(err)
 	}
 
-	buf, err := s.gt.generate(content.TypePlain, t)
+	w := new(testWriter)
+
+	err = s.gt.Generate(t, w)
 	if assert.NoError(s.T(), err) {
-		assert.Equal(s.T(), expectedBuffer, buf)
+		assert.Equal(s.T(), expectedBuffer, w.buf)
 	}
 }
 
