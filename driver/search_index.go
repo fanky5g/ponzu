df4f7a8638035a05e5499c5920161885874c689f
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-05-20 11:08:10 +0200

    differentiate search with pagination

--- driver/search_index.go
+++ driver/search_index.go
@@ -7,7 +7,6 @@ type SearchIndexInterface interface {
 }
 
 type SearchInterface interface {
-	Update(id string, data interface{}) error
-	Delete(id string) error
-	Search(query string, count, offset int) ([]interface{}, int, error)
+	SearchIndexInterface
+	SearchWithPagination(query string, count, offset int) ([]interface{}, int, error)
 }
