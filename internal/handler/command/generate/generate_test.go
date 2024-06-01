2f9727a7136bf4e2b2dc400cdead423b3e95defa
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-06-01 11:12:07 +0200

    update go.mod.tmpl use recent release

--- internal/handler/command/generate/generate_test.go
+++ internal/handler/command/generate/generate_test.go
@@ -159,7 +159,7 @@ func (suite *GenerateTestSuite) TestWriteTemplateGoMod() {
 go 1.16.0
 
 require (
-	github.com/fanky5g/ponzu v0.0.0-20240505100750-c91a29f57604
+	github.com/fanky5g/ponzu v0.0.0-20240601084401-39b2b06d3406
 )`)
 	}
 }
