a35207ed0a748673fce823ef20d16268ebabaff8
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2024-09-29 07:25:51 +0200

    remove commented code for sort

--- internal/handler/controllers/contents_handler.go
+++ internal/handler/controllers/contents_handler.go
@@ -70,39 +70,6 @@ func NewContentsHandler(r router.Router) http.HandlerFunc {
 		//		html := `<div class="col s9 card">
 		//					<div class="card-content">
 		//					<div class="row">
-		//					<div class="col s8">
-		//						<div class="row">
-		//							<div class="col s5 input-field inline">
-		//								<select class="browser-default __ponzu sort-order">
-		//									<option value="DESC">New to Old</option>
-		//									<option value="ASC">Old to New</option>
-		//								</select>
-		//								<label class="active">Sort:</label>
-		//							</div>
-		//							<script>
-		//								$(function() {
-		//									var sort = $('select.__ponzu.sort-order');
-		//
-		//									sort.on('change', function() {
-		//										var path = window.location.pathname;
-		//										var s = sort.val();
-		//										var t = getParam('type');
-		//
-		//										if (status == "") {
-		//											status = "public";
-		//										}
-		//
-		//										window.location.replace(path + '?type=' + t + '&order=' + s);
-		//									});
-		//
-		//									var order = getParam('order');
-		//									if (order !== '') {
-		//										sort.val(order);
-		//									}
-		//
-		//								});
-		//							</script>
-		//						</div>
 		//					</div>
 		//					<form class="col s4" action="{{ .PublicPath }}/contents/search" method="get">
 		//						<div class="input-field post-search inline">
