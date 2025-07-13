a8d86b7bc2713f586d9a16f8214a2615e9996a40
Benjamin Appiah-Brobbey <fanky5g@gmail.com>
2025-07-13 04:15:25 +0200

    gcs write file content type attribute

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -6,8 +6,12 @@ import (
 )
 
 type Client interface {
-	// Save saves a file to storage. Make sure to close file after it's written
 	Save(fileName string, file io.ReadCloser) (string, int64, error)
 	Delete(path string) error
 	Open(name string) (http.File, error)
+	Attributes(name string) (*FileAttributes, error)
+}
+
+type FileAttributes struct {
+	ContentType string
 }
